70fcf760a42632c9cf471968993b45d66807a1a0
Justinbecec1 <Justinildefonso@gmail.com>
2025-02-04 15:40:49 -0500

    I added changes to the components

---
+++ src/components/Content.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Content = () => {
+return (
+<p>The story of the Tortoise and the Hare is a classic, one day the hare was making fun of the tortoise for being slow, the tortoise warned the hare about him being faster than he seems, the hare scoffed and offered the tortoise a race, the hare didn't take the race seriously and even napped during the race, this resulted in the tortoise winning.</p>
+)
+};
+export default Content;
---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => {
+return (
+<footer>
+<h2>Moral of the story: doing things fast doesn't always result in victory, sometimes the slow way wins the race.</h2>
+</footer>
+)
+}
+export default Footer;
